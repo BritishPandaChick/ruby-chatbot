cbe68c9d13142cd87c832999b7d942c8445868f0
BritishPandaChick <sbartley1080@gmail.com>
2022-01-31 21:06:51 -0500

    Fix socket variable

--- chatbot.rb
+++ chatbot.rb
@@ -1,16 +1,16 @@
 require 'socket'
- 
+
 server  = 'teepee.freenode.net'
 port    = 6667
-socket  = TCPSocket.open(server, port)
- 
+socket = TCPSocket.open(server, port)
+
 nickname = 'SkillcrushBotOMG'
 channel  = '#BananaStand123'
- 
+
 socket.puts "NICK #{nickname}"
 socket.puts "USER #{nickname} 0 * #{nickname}"
 socket.puts "JOIN #{channel}"
- 
+
 while message = socket.gets do
   puts message
 
@@ -19,4 +19,4 @@ while message = socket.gets do
     puts "PONG #{server}"
     socket.puts "PONG #{server}"
   end
-end 
+end
